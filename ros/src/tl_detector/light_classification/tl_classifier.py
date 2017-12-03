9e50d2a06d387dfc3b23738d9ec0cd48118be757
Will Buxton <will.buxton88@gmail.com>
2017-12-02 23:28:35 -0800

    tl_classifier uses the model created in tl_trainer

--- ros/src/tl_detector/light_classification/tl_classifier.py
+++ ros/src/tl_detector/light_classification/tl_classifier.py
@@ -1,11 +1,16 @@
 from styx_msgs.msg import TrafficLight
+from keras.models import load_model
+from keras.preprocessing import image
+import numpy as np
+import cv2
 
 class TLClassifier(object):
     def __init__(self):
         #TODO load classifier
+        self.model = load_model("model.h5")
         pass
 
-    def get_classification(self, image):
+    def get_classification(self, img):
         """Determines the color of the traffic light in the image
 
         Args:
@@ -15,5 +20,11 @@ class TLClassifier(object):
             int: ID of traffic light color (specified in styx_msgs/TrafficLight)
 
         """
-        #TODO implement light color prediction
-        return TrafficLight.UNKNOWN
+        # TODO implement light color prediction
+        img = cv2.resize(img, (600, 800))
+        x = image.img_to_array(img)
+        x = np.expand_dims(x, axis=0)
+        pred = self.model.predict(x)
+        if pred:
+            return TrafficLight.Green
+        return TrafficLight.Red
