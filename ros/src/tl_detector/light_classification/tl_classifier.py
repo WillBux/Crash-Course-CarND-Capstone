ef9bec41d770388011ab7be9a327cffac6779024
bharatbobba <bharatbobba@gmail.com>
2017-12-20 11:58:42 -0500

    Updated TL CLassification

--- ros/src/tl_detector/light_classification/tl_classifier.py
+++ ros/src/tl_detector/light_classification/tl_classifier.py
@@ -2,20 +2,32 @@ from styx_msgs.msg import TrafficLight
 import numpy as np
 import cv2
 import rospy
-try:
-    from keras.models import load_model
-    from keras.preprocessing import image
-except ImportError:
-    rospy.logwarn("KERAS could not be imported!")
+import time
+import os
+import sys
+import tensorflow as tf
+from io import StringIO
+from collections import defaultdict
+PATH_TO_MODEL = 'tf_dependencies/frozen_inference_graph.pb'
 
 class TLClassifier(object):
     def __init__(self):
-        #TODO load classifier
-        try:
-            self.model = load_model("model.h5")
-        except:
-            rospy.logwarn("Classifier model could not be loaded")
-        pass
+        self.detection_graph = tf.Graph()
+        with self.detection_graph.as_default():
+            od_graph_def = tf.GraphDef()
+            # Works up to here.
+            with tf.gfile.GFile(PATH_TO_MODEL, 'rb') as fid:
+                serialized_graph = fid.read()
+                od_graph_def.ParseFromString(serialized_graph)
+                tf.import_graph_def(od_graph_def, name='')
+        self.image_tensor = self.detection_graph.get_tensor_by_name('image_tensor:0')
+        self.detection_boxes = self.detection_graph.get_tensor_by_name('detection_boxes:0')
+        self.detection_scores = self.detection_graph.get_tensor_by_name('detection_scores:0')
+        self.detection_classes = self.detection_graph.get_tensor_by_name('detection_classes:0')
+        self.num_detections = self.detection_graph.get_tensor_by_name('num_detections:0')
+        self.sess = tf.Session(graph=self.detection_graph)
+        self.current_light = TrafficLight.UNKNOWN
+
 
     def get_classification(self, img):
         """Determines the color of the traffic light in the image
@@ -27,19 +39,27 @@ class TLClassifier(object):
             int: ID of traffic light color (specified in styx_msgs/TrafficLight)
 
         """
-        # TODO implement light color prediction
+
         try:
-            img = cv2.resize(img, (600, 800))
-            x = image.img_to_array(img)
-            x = np.expand_dims(x, axis=0)
-            pred = self.model.predict(x)
-            color = np.argmax(pred)
-            if color == 2:
-                return TrafficLight.Green
-            if color == 1:
-                return TrafficLight.Yellow
-            if color == 2:
-                return TrafficLight.Red
+            with self.detection_graph.as_default():
+            # Expand dimension since the model expects image to have shape [1, None, None, 3].
+            img_expanded = np.expand_dims(img, axis=0)
+            (boxes, scores, classes, num) = self.sess.run(
+                [self.detection_boxes, self.detection_scores, self.detection_classes, self.num_detections],
+                feed_dict={self.image_tensor: img_expanded})
+
+            boxes = np.squeeze(boxes)
+            scores = np.squeeze(scores)
+            classes = np.squeeze(classes).astype(np.int32)
+
+            min_score_threshold = 0.5
+            if scores[0] > min_score_threshold:
+                if classes[0] == 1:
+                    return TrafficLight.GREEN
+                elif classes[0] == 2:
+                    return TrafficLight.YELLOW
+                elif classes[0] == 3:
+                    return TrafficLight.RED
             else:
                 return TrafficLight.UNKNOWN
         except:
